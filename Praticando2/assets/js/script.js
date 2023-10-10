2e4161e1080d8a7cdac0d420d77b67816156890c
Cynthia Padilha <cynthiarfsilva@gmail.com>
2023-10-09 21:24:00 -0300

    relogio digital

---
+++ Praticando2/assets/js/script.js
@@ -0,0 +1,20 @@
+const horas = document.getElementById('horas');
+const minutos = document.getElementById('minutos');
+const segundos = document.getElementById('segundos');
+
+const relogio = setInterval(function time (){
+    let dateToday = new Date();
+    let h = dateToday.getHours();
+    let m = dateToday.getMinutes();
+    let s = dateToday.getSeconds();
+
+    if (h < 10) h = '0' + h;
+
+    if (m < 10) m = '0' + m;
+
+    if (s < 10) s = '0' + s;
+
+    horas.textContent = h;
+    minutos.textContent = m;
+    segundos.textContent = s;
+})
